1d5693b234d8ada733dc863237725e6ea2522433
Patrick Rogers <225264952+codewithprogers@users.noreply.github.com>
2026-07-05 22:33:52 -0700

    feat: create initial console game logic
    Create the game controller with two players and alternating turns. Add
    console board output and prevent players from placing markers in
    occupied cells.

--- script.js
+++ script.js
@@ -15,14 +15,23 @@ function Gameboard() {
   const placeMarker = (chosenRow, chosenColumn, player) => {
     const selectedCell = board[chosenRow][chosenColumn];
 
-    if (selectedCell.getValue() !== "") return;
+    if (selectedCell.getValue() !== "") return false;
 
     selectedCell.addMarker(player);
+    return true;
+  };
+
+  const printBoard = () => {
+    const boardWithCellValues = board.map((row) =>
+      row.map((cell) => cell.getValue()),
+    );
+    console.log(boardWithCellValues);
   };
 
   return {
     getBoard,
     placeMarker,
+    printBoard,
   };
 }
 
@@ -40,3 +49,58 @@ function Cell() {
     getValue,
   };
 }
+
+function GameController(
+  playerOneName = "Player One",
+  playerTwoName = "Player Two",
+) {
+  const board = Gameboard();
+
+  const players = [
+    {
+      name: playerOneName,
+      marker: "X",
+    },
+    {
+      name: playerTwoName,
+      marker: "O",
+    },
+  ];
+
+  let activePlayer = players[0];
+
+  const switchPlayerTurn = () => {
+    activePlayer = activePlayer === players[0] ? players[1] : players[0];
+  };
+  const getActivePlayer = () => activePlayer;
+
+  const printNewRound = () => {
+    board.printBoard();
+    console.log(`${getActivePlayer().name}'s turn.`);
+  };
+
+  const playRound = (row, column) => {
+    console.log(
+      `Placing ${getActivePlayer().name}'s marker into row ${row}, column ${column}.`,
+    );
+    const moveWasSuccessful = board.placeMarker(row, column, getActivePlayer().marker);
+
+    if (!moveWasSuccessful) {
+      console.log("That spot is already taken.");
+      return;
+    }
+    // This is where the check for a winner, loser, or tie and handle that logic, such as a win message.
+
+    switchPlayerTurn();
+    printNewRound();
+  };
+
+  printNewRound();
+
+  return {
+    playRound,
+    getActivePlayer,
+  };
+}
+
+const game = GameController();
